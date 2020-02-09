52d451c3024b69ad9a69564b37f68a91344c58d3
EnderDev <xenderpugx@gmail.com>
2020-02-09 16:48:01 +0000

    💄 Add menu to icons

--- settings/src/renderer/settings/constants/icons.ts
+++ settings/src/renderer/settings/constants/icons.ts
@@ -4,6 +4,7 @@ import * as downloads from '../../../shared/icons/download.svg'
 import * as languages from '../../../shared/icons/translate.svg'
 import * as info from '../../../shared/icons/info.svg'
 import * as feedback from '../../../shared/icons/feedback.svg'
+import * as menu from '../../../shared/icons/menu.svg'
 
 import * as favicon from '../../../shared/icons/settings.ico'
 import * as defaultAvatar from '../../../shared/icons/default.svg'
@@ -16,7 +17,8 @@ export const icons = {
     info: info.default,
     feedback: feedback.default,
     defaultAvatar: defaultAvatar.default,
-    favicon: favicon.default
+    favicon: favicon.default,
+    menu: menu.default
 }
 
 console.log(icons)
